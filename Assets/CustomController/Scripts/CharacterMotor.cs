d5b1b4e32ea08910a81bd121a8d30a8254311c5b
AlexRak2 <alejandrorak2@gmail.com>
2023-09-17 13:28:13 -0400

    Removed Random Notes

--- Assets/CustomController/Scripts/CharacterMotor.cs
+++ Assets/CustomController/Scripts/CharacterMotor.cs
@@ -1,25 +1,4 @@
-/// <summary>
-/// Third Person Controller
-/// <copyright>(c) AlexRak2TheDev or Alejandro Hernandez 2013</copyright>
-/// 
-/// Third Person Controller homepage: https://github.com/AlexRak2/PlayerControllerTemplates
-/// 
-/// This software is provided 'as-is', without any express or implied
-/// warranty.  In no event will the authors be held liable for any damages
-/// arising from the use of this software.
-///
-/// Permission is NOT granted to anyone to use this software for any purpose,
-/// and to alter it and redistribute it freely.
-///
-/// 1. The origin of this software must not be misrepresented; you must not
-/// claim that you wrote the original software. If you use this software
-/// in a product, an acknowledgment in the product documentation would be
-/// appreciated but is not required.
-/// 2. Altered source versions must be plainly marked as such, and must not be
-/// misrepresented as being the original software.
-/// 3. This notice may not be removed or altered from any source distribution.
-/// </summary>
-/// 
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 # There will most likely be bugs as i never finished it. I believe its a good starter point with some easy non complex code to continue off
 
 # if you have any questions feel free to join my discord and ask me! https://discord.com/invite/vB2NCBN44r
+
+# This package contains a third person template, with the following features:
+
+# walk / run states
+# multi direction strafing
+# jumping / unique animation for hard langing
+# camera shoulder swap
+# aiming (camera)
+# IK Foot (Started but never finished)
